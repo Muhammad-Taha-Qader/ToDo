490e70d669d83170a27e19cc254c1b005ca04bae
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-07-16 19:08:42 +0000

    replace static text items with dynamic input

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ function addNewTodoItem(){
     newItemCheckbox.type = 'checkbox';
     newItemCheckbox.setAttribute('onchange' , 'handleCheckboxChange(this)');
 
-    //Creat new lable text for iteam
-    let newItemLable = document.createElement('label');
-    newItemLable.setAttribute('for', `${newItemCheckbox.id}`);
-    newItemLable.textContent=`your ${itemsAvliableSoFar} task`;
+    //Creat new textarea for iteam
+    let newItemTextarea = document.createElement('textarea');
+    newItemTextarea.setAttribute('data-for', `${newItemCheckbox.id}`);
+    // newItemLable.textContent=`your ${itemsAvliableSoFar} task`;
+    newItemTextarea.setAttribute('placeholder','Enter description...');
+    newItemTextarea.setAttribute('oninput','autoResize(this)');
 
+    //Creat new del btn for iteam
     let newIteamdel=document.createElement('button');
     newIteamdel.setAttribute('onclick','removeCurrentTodoItem(this)');
     newIteamdel.innerHTML="<i class='fas fa-trash'></i>";
@@ -23,12 +26,13 @@ function addNewTodoItem(){
 
     //Putting chcek box and labes inside the div that we created
     newTodoItem.appendChild(newItemCheckbox);
-    newTodoItem.appendChild(newItemLable);
+    newTodoItem.appendChild(newItemTextarea);
     newTodoItem.appendChild(newIteamdel);
 
     //putting the div(new to do item) in our main todoIteams list/card
     // document.getElementsByClassName("todoCard")[0].appendChild(newTodoItem);
-    document.getElementsByClassName("todoItemUnchecked")[0].appendChild(newTodoItem);
+   // Instead this use following document.getElementsByClassName("todoItemUnchecked")[0].appendChild(newTodoItem);
+    document.querySelector("todoItemUnchecked").appendChild(newTodoItem);
 
     //NO need now as previously new items were in todoCard so we have to reposition the add butoon but now every newiteam in in UNCKED div
     // //But add button should go down the newly created iteam so:
@@ -98,3 +102,35 @@ function handleCheckboxChange(checkbox) {
         // Do something when the checkbox is not checked
     }
 } */}
+
+
+
+
+function autoResize(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+}
+
+
+function togalTheme(){
+    let todoItemUnchecked=document.getElementsByClassName('todoItemUnchecked')[0];
+    let todoItemChecked=document.getElementsByClassName('todoItemChecked')[0];
+    todoItemUnchecked.classList.remove('bg-slate-500');
+    todoItemUnchecked.classList.add('bg-purple-900');
+
+    todoItemChecked.classList.remove('bg-green-400');
+    todoItemChecked.classList.add('bg-fuchsia-900');
+
+    // let myHtml= document.getElementsByTagName('html')[0];
+    // myHtml.classList.remove('bg-gradient-to-r from-purple-950 to-fuchsia-900');
+    // console.log(myHtml.classList);
+    // myHtml.classList.add('bg-gradient-to-r', 'from-slate-800', 'to-slate-900');
+
+    let myHtml = document.getElementsByTagName('html')[0];
+    let classesToRemove = ['bg-gradient-to-r', 'from-purple-950', 'to-fuchsia-900'];
+    classesToRemove.forEach(className => myHtml.classList.remove(className));
+    console.log(myHtml.classList);
+    myHtml.classList.add('bg-gradient-to-r', 'from-slate-800', 'to-slate-900');
+
+
+}
