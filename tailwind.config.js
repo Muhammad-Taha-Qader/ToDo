f8ded65a3bc9ba86632ada17e3b8ffc23539817f
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-07-18 16:25:26 +0500

    New item creation flow bug fix + UI changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
@@ -113,6 +113,6 @@ module.exports = {
     },
 
   ],
-}
+};
 
 
